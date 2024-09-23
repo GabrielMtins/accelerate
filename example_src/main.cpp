0d9b482de61b5bf49e9b706ea029547d534b4b19
gabrielmtins <gabrielmartinsthe12381@gmail.com>
2024-09-23 18:45:08 -0300

    modificado makefile e adicionado sistemas de bibliotecas

--- example_src/main.cpp
+++ example_src/main.cpp
@@ -1,17 +1,9 @@
-#include <iostream>
-
-#include "Scene2d.hpp"
 #include "Game.hpp"
-#include "ComponentArray.hpp"
-#include "ComponentManager.hpp"
-#include "SystemManager.hpp"
+#include "Scene2d.hpp"
 
 int main(int argc, char **argv){
-	(void) argc;
-	(void) argv;
-
 	acc::Game *game;
-	
+
 	game = new acc::Game("oi", 400, 240);
 
 	game->setScene((acc::Scene *) new acc::Scene2d(game));
